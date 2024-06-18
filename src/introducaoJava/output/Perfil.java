6ad6416b8b6bd7d58806912877e5af08e056a160
Davi Ribeiro de Souza <Davi.rsouz@outlook.com>
2024-06-18 19:51:47 -0300

    Introdução ao Java

---
+++ src/introducaoJava/output/Perfil.java
@@ -0,0 +1,8 @@
+package introducaoJava.output;
+
+public class Perfil{
+    public static void main(String[] args) {
+        System.out.println("Olá, Davi");
+        System.out.println("Tudo bem?");
+    }
+}
