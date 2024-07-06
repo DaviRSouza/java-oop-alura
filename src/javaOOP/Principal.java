2f1aad658b74a428a30da1e67d806ee840c29b75
DaviRSouza <davi.rsouz@outlook.com>
2024-07-06 14:54:55 -0300

    Atribuindo valores ao nossso filme

---
+++ src/javaOOP/Principal.java
@@ -0,0 +1,11 @@
+package javaOOP;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Poderoso Chefão";
+        meuFilme.anoDeLancamento = 1972;
+        meuFilme.duracaoEmMinutos = 175;
+
+    }
+}
