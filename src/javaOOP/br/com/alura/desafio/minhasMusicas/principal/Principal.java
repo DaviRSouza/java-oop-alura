41e4c75c96e5a4019a713282f18c77a1aac7b950
DaviRSouza <davi.rsouz@outlook.com>
2024-07-10 03:05:11 -0300

    Desafio Java Orientado a Objetos

---
+++ src/javaOOP/br/com/alura/desafio/minhasMusicas/principal/Principal.java
@@ -0,0 +1,37 @@
+package javaOOP.br.com.alura.desafio.minhasMusicas.principal;
+
+import javaOOP.br.com.alura.desafio.minhasMusicas.modelos.MinhasPreferidas;
+import javaOOP.br.com.alura.desafio.minhasMusicas.modelos.Musica;
+import javaOOP.br.com.alura.desafio.minhasMusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
+}
